99da00600944417fa786e1758e3937009505aec9
Galih Setyawan <galih.kamvax032@gmail.com>
2023-09-17 23:00:18 +0700

    fix:dockerfile

--- config/config.js
+++ config/config.js
@@ -26,11 +26,11 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectOptions: {
       bigNumberStrings: true,
