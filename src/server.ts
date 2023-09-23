4034777d2bfe169da748b34053f273d08a00a290
Galih Setyawan <galih.kamvax032@gmail.com>
2023-09-23 21:09:59 +0700

    add external route

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import MainRouter from "./routes";
 import AuthRouter from "./routes/auth";
 import OrderRouter from "./routes/order";
+import ExternalRouter from "./routes/external";
 
 export default class Server {
   expressInstance: express.Express;
@@ -36,11 +37,12 @@ export default class Server {
     let router = new MainRouter().router;
     let authRouter = new AuthRouter().router;
     let orderRouter = new OrderRouter().router;
+    let externalRoute = new ExternalRouter().router;
 
     // Add to server routes
     this.expressInstance.use("/", router);
     this.expressInstance.use("/auth", authRouter);
     this.expressInstance.use("/order", orderRouter);
-    
+    this.expressInstance.use("/external", externalRoute);
   }
 }
