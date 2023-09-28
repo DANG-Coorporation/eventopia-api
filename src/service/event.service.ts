ae707738b9efc58230cb37a7525ce5782d265685
AndiYusriandiPratama <andiyusriandiprtm@gmail.com>
2023-09-28 21:44:15 +0800

    add: create eventDetailById & uniqId

--- src/service/event.service.ts
+++ src/service/event.service.ts
@@ -4,9 +4,14 @@ import Event, {
   EventCreationAttributes,
 } from "../database/models/event";
 import EventTickets from "../database/models/eventTicket";
+import { NotFoundException } from "../helper/Error/NotFound/NotFoundException";
 import { removeLimitAndPage } from "../helper/function/filteredData";
 import { getUniqId } from "../helper/function/getUniqId";
 import { IPaginate } from "../helper/interface/paginate/paginate.interface";
+
+interface EventDetail extends EventAttributes {
+  isExpired: boolean;
+}
 
 export default class EventService {
   async create(input: EventAttributes) {
@@ -42,6 +47,16 @@ export default class EventService {
     }
   }
 
+  async getEventById(id: number) {
+    try {
+      const event = await Event.findByPk(id);
+      if (!event) throw new NotFoundException("Event not Found", {});
+      return event;
+    } catch (error) {
+      throw new Error(`Error While fetching eventId: ${error}`);
+    }
+  }
+
   async page(input: IPaginate<EventAttributes>) {
     try {
       const page = input.page ?? 1;
@@ -74,6 +89,29 @@ export default class EventService {
       return events;
     } catch (error) {
       console.error("Error mendapatkan pagination", error);
+      throw error;
+    }
+  }
+
+  async getEventDetailByUniqId(uniqId: string) {
+    try {
+      // Cari acara berdasarkan uniqId
+      const event = await Event.findOne({
+        where: { uniqueId: uniqId },
+      });
+
+      if (!event) {
+        throw new Error("Acara tidak ditemukan");
+      }
+
+      // Periksa apakah tanggal acara lebih besar dari tanggal saat ini
+      const currentDate = new Date();
+      const eventDate = new Date(event.eventStartDateTime);
+
+      const isExpired = eventDate <= currentDate;
+
+      return { ...event.toJSON(), isExpired } as EventDetail;
+    } catch (error) {
       throw error;
     }
   }
